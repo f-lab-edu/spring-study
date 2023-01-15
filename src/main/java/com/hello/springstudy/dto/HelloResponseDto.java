2411cad2d639b73f516176ece1babc46da0a5f04
LEEEUNJI <eunjilee3008@gmail.com>
2023-01-15 20:57:25 +0900

    feat: hello API 추가

---
+++ src/main/java/com/hello/springstudy/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.hello.springstudy.dto;
+
+public class HelloResponseDto {
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
